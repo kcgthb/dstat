ddd97d7c49a82c590d378e6dcf47d03086e3c5cb
Kilian Cavalotti <kilian@stanford.edu>
2014-08-20 16:22:06 -0700

    dstat_lustre: fix broken Lustre plugin (add discover(), fix metrics updating and author's name)

--- plugins/dstat_lustre.py
+++ plugins/dstat_lustre.py
@@ -1,33 +1,47 @@
-# Author: Brock Palen <brockp@mlds-networks.com>, Kilian Vavalotti <kilian@stanford.edu>
+### Author: Kilian Cavalotti <kilian@stanford.edu>, Brock Palen <brockp@mlds-networks.com>
 
 class dstat_plugin(dstat):
+    """
+    Read and write throughput (in bytes) on per Lustre filesystem.
+    """
     def __init__(self):
         self.nick = ('read', 'write')
+        self.cols = 2
+
+    def discover(self, *objlist):
+        ret = []
+        for fs in os.listdir('/proc/fs/lustre/llite'):
+            name = fs.split("-")[0]
+            ret.append(name)
+        for item in objlist: ret.append(item)
+        if not ret:
+            raise Exception, "No suitable Lustre filesystem found to monitor"
+        return ret
 
     def check(self):
         if not os.path.exists('/proc/fs/lustre/llite'):
             raise Exception, 'Lustre filesystem not found'
-        info(1, 'Module %s is still experimental.' % self.filename)
 
     def name(self):
-        return [mount for mount in os.listdir('/proc/fs/lustre/llite')]
+        return [fs.split("-")[0] for fs in os.listdir('/proc/fs/lustre/llite')]
 
     def vars(self):
-        return [mount for mount in os.listdir('/proc/fs/lustre/llite')]
+        return [fs for fs in os.listdir('/proc/fs/lustre/llite')]
 
     def extract(self):
-        for name in self.vars:
-            for l in open(os.path.join('/proc/fs/lustre/llite', name, 'stats')).splitlines():
-                if len(l) < 6: continue
+        for fs in self.vars:
+            for line in open(os.path.join('/proc/fs/lustre/llite', fs, 'stats')).readlines():
+                l = line.split()
+                if len(line) < 6: continue
                 if l[0] == 'read_bytes':
                     read = long(l[6])
-                elif l[0] == 'write_bytes':
+                if l[0] == 'write_bytes':
                     write = long(l[6])
-            self.set2[name] = (read, write)
+            self.set2[fs] = (read, write)
 
-            self.val[name] = map(lambda x, y: (y - x) * 1.0 / elapsed, self.set1[name], self.set2[name])
+            self.val[fs] = map(lambda x, y: (y - x) * 1.0 / elapsed, self.set1[fs], self.set2[fs])
 
-        if step == op.delay:
-            self.set1.update(self.set2)
+            if step == op.delay:
+                self.set1.update(self.set2)
 
 # vim:ts=4:sw=4
